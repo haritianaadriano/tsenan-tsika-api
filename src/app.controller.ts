8820099da92b2e204cd2bb1a8942648d314d8c1a
Haritiana Adriano <hei.adriano.4@gmail.com>
2024-01-23 10:23:54 +0300

    chore: GET /ping to return pong

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,7 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getPing(): string {
+    return this.appService.getPing();
   }
 }
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -15,10 +15,10 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
+  it('/ping (GET)', () => {
     return request(app.getHttpServer())
-      .get('/')
+      .get('/ping')
       .expect(200)
-      .expect('Hello World!');
+      .expect('pong');
   });
 });
