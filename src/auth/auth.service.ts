33ad32d4a3c3ac4695fd76d06bf0815c88eca9bd
Haritiana Adriano <hei.adriano.4@gmail.com>
2024-01-30 19:27:59 +0300

    chore: use jwt as token provider

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,17 +1,26 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
 import { UserService } from 'src/service/user.service';
 
 @Injectable()
 export class AuthService {
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+  ) {}
 
-  async signIn(email: string, password: string): Promise<any> {
+  async signIn(
+    email: string,
+    password: string,
+  ): Promise<{ email: string; token: string }> {
     const user = await this.userService.findOne(email);
     if (user?.password !== password) {
       throw new UnauthorizedException();
     }
-    // TODO: Generate jwt return it here
-
-    return user;
+    const payload = { id: user.id, username: user.username };
+    return {
+      email: user.email,
+      token: await this.jwtService.signAsync(payload),
+    };
   }
 }
--- src/module/auth.module.ts
+++ src/module/auth.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from '../auth/auth.controller';
 import { AuthService } from '../auth/auth.service';
-import { UserService } from 'src/service/user.service';
 import { UserModule } from './user.module';
+import { JwtModule } from '@nestjs/jwt';
+import { jwtConstant } from 'src/auth/constant';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    JwtModule.register({
+      global: true,
+      secret: jwtConstant.secret,
+      signOptions: { expiresIn: '3600s' },
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
