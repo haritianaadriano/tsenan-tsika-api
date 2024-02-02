41807651a66c1afb4853f5ec6b65eebb4210ddcf
Haritiana Adriano <hei.adriano.4@gmail.com>
2024-02-02 16:37:28 +0300

    docs: init authentification api documentation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { Public } from './../module/decorator/public-access.decorator';
+
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
@@ -10,4 +11,8 @@ export class AuthController {
   signIn(@Body() signIn: Record<string, any>) {
     return this.authService.signIn(signIn.email, signIn.password);
   }
+
+  @Public()
+  @Post('/signup')
+  signUp(@Body() toCreate: )
 }
