30ed978fb0d5d7c7cb634d9914b2a870c89e7ab4
Haritiana Adriano <hei.adriano.4@gmail.com>
2024-02-05 14:07:49 +0300

    fix: auto generate user id

--- src/model/user.entity.ts
+++ src/model/user.entity.ts
@@ -1,8 +1,8 @@
-import { Column, Entity, PrimaryColumn } from 'typeorm';
+import { Column, Entity, PrimaryColumn, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
 export class User {
-  @PrimaryColumn()
+  @PrimaryGeneratedColumn()
   id: string;
   @Column({ nullable: true })
   username: string;
